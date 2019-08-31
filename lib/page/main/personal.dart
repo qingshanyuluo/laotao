1eb4e6388966da1f49b581e4ac8c6b06ec68d098
qingshanyuluo <qingshanyuluo@gmail.com>
2019-08-31 18:37:06 +0800

    个人中心

--- lib/page/main/personal.dart
+++ lib/page/main/personal.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lao_tao/main.dart';
 import 'package:lao_tao/page/login_and_register.dart';
@@ -11,86 +11,63 @@ class PersonalPage extends StatefulWidget {
 
 class _PersonalPageState extends State<PersonalPage> {
   TextEditingController typeController = TextEditingController();
-  String buttonText = "hhhhhhhhhh";
+  String usename = "尚未登录";
   @override
   Widget build(BuildContext context) {
     // print(getShareDate("key"));
-    getShareDate("key").then((val){
+    getShareDate("username").then((val){
       print(val);
+      setState(() {
+        usename = val;
+      });
     });
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("演示post提交数据"),
+          title: Text("我的"),
         ),
         body: Container(
+          color: Colors.grey,
           child: Center(
             child: Column(
               children: <Widget>[
-                Text('ddddddd演示一下 为了登录', style: TextStyle(backgroundColor: Colors.black, color: Colors.blue)),
-                Text('ddddddd演示一下 为了登录', style: TextStyle(backgroundColor: Colors.black, color: Colors.blue)),
-                Text('ddddddd演示一下 为了登录', style: TextStyle(backgroundColor: Colors.black, color: Colors.blue)),
-                Text('ddddddd演示一下 为了登录', style: TextStyle(backgroundColor: Colors.black, color: Colors.blue)),
-                Text('用户名xxxx是对的', style: TextStyle(backgroundColor: Colors.black, color: Colors.blue)),
-                TextField(
-                  controller: typeController,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(20.0),
-                    labelText: '请输入准备发送的文本',
-                    helperText: '一段小帮助'
-                  ),
-                  autofocus: false,
-                ),
-                RaisedButton(
-                  onPressed: _choiceAction,
-                  color: Colors.blue,
-                  child: Text(buttonText),
-                ),
-                RaisedButton(
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context){
-                        return LandRPage();
-                      }
-                    ));
-                  },
-                  color: Colors.blue,
-                  child: Text('登录'),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                    Container(child: Image.asset("img/tou.png"),padding: EdgeInsets.fromLTRB(10, 3, 3, 3),),
+                    Container(child: Container(
+                      // padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("小甜甜", style: TextStyle(fontSize: 25),),
+                          Text("账号: " + usename, style: TextStyle(fontSize: 15),)
+                        ],
+                      ),
+                    ),),
+                    IconButton(
+                      icon: Icon(CupertinoIcons.right_chevron),
+                      onPressed: login,
+                    )
+                    
+                  ],),
                 )
-              ]
-             ),
-           ),
+              ],    
+            ),
+          ),
          ),
        ),
     );
   }
 
-    void _choiceAction(){
-    print("jjjjjjjjjjjjjjjjjjjjjj");
-    if (typeController.text.toString() == '') {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(title: Text('空'),)
-      );
-    } else {
-      getHttp(typeController.text.toString()).then((val){
-        setState(() {
-          print(val);
-          buttonText = val.toString();
-        });
-      });
-    }
-  }
-
-  Future getHttp(String typedText) async{
-    try {
-      Response response;
-      var data = {'username': typeController.text.toString(), 'password': 'xxxxqq'};
-      response = await Dio().post("http://10.0.2.2:8080/test",queryParameters: data);
-      print(response);
-      return response.data;
-    } catch (e) {
-      print(e);
-    } 
+  void login(){
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context){
+        return LandRPage();
+      }
+    ));
   }
 }
