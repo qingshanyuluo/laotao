66bfe79ab7c536e88b16f3f6cbe0eb646a0b4c84
qingshanyuluo <qingshanyuluo@gmail.com>
2019-09-03 16:44:11 +0800

    简单完成上传

--- lib/page/main/add.dart
+++ lib/page/main/add.dart
@@ -13,12 +13,11 @@ class AddPage extends StatelessWidget {
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text("添加好店"),),
-        body: Center(child: Container(
+        body: Container(
           child: HeadImageUploadPage(),
           )
-        ) 
-      ),
-    );
+        )
+      );
   }
 }
 
@@ -30,26 +29,46 @@ class HeadImageUploadPage extends StatefulWidget {
 }
 
 class _HeadImageUploadPageState extends State<HeadImageUploadPage> {
-  File _image;
+  // File _image;
+  List<File> _images = new List();
+  int i;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-    _upLoadImage(image);//上传图片
     setState(() {
-      _image = image;
+      _images.add(image);
     });
+    // _upLoadImage(_image);//上传图片
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker Example'),
-      ),
+      // body: Center(
+      //   child: _image == null
+      //       ? Text('No image selected.')
+      //       : Image.file(_image),
+      // ),
       body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
+        child: _images.length==0
+        ? Text("jjj")
+        :
+        Row(
+          children: <Widget>[
+            Column(
+              children: _images.map((file){
+                return Image.file(file, height: 20,);
+              }).toList(),
+            ),
+            RaisedButton(
+              onPressed: (){
+                _upLoadImage(_images);
+              },
+            )
+          ],
+        )
+       
+        
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
@@ -59,14 +78,27 @@ class _HeadImageUploadPageState extends State<HeadImageUploadPage> {
     );
   }
 //上传图片
-  _upLoadImage(File image) async {
-    String path = image.path;
+  _upLoadImage(List<File> image) async {
+    String path = image[0].path;
+    print(path);
     var name = path.substring(path.lastIndexOf("/") + 1, path.length);
-    FormData formData = new FormData.from({
-      "file": new UploadFileInfo(new File(path), name)
+    // FormData formData = new FormData.from({
+    //   "file": new UploadFileInfo(new File(path), name),
+    // });
+    FormData formData = FormData.from({
+      "file": [
+        // UploadFileInfo(File("./example/upload.txt"), "upload.txt"),
+        UploadFileInfo(File(path), name),
+        UploadFileInfo(File(path), name),
+        // "jfldfjlsafjol",
+        // "jfldfjlsafjol",
+        // "jfldfjlsafjol",
+      ]      
     });
     Dio dio = new Dio();
-    var respone = await dio.post<String>("路径", data: formData);
+    var respone = await dio.post<String>("http://lennon.xyz/uploadShop/1?name=火鸡的夫&cost=34&location=天津&commit=警方答飞沙芬按时发货经费和四放哈师大封杀康复后的萨科&type=烤鸭打撒十大", data: formData);
+    // var respone = await dio.post<String>("http://lennon.xyz/uploadShop/1?name=火鸡的夫&cost=34&location=天津&commit=警方答飞沙芬按时发货经费和四放哈师大封杀康复后的萨科&type=烤鸭打撒十大");
+    print(respone);
     if (respone.statusCode == 200) {
       Fluttertoast.showToast(
           msg: "图片上传成功",
@@ -75,71 +107,3 @@ class _HeadImageUploadPageState extends State<HeadImageUploadPage> {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-// class WeiboPage extends StatefulWidget {
-//   WeiboPage({Key key}) : super(key: key);
-
-//   _WeiboPageState createState() => _WeiboPageState();
-// }
-
-// class _WeiboPageState extends State<WeiboPage> {
-//   TextEditingController typeController = TextEditingController();
-//   String buttonText = "hhhhhhhhhh";
-//   String showText = "hhhhhhhhhh";
-  
-//   @override
-//     Widget build(BuildContext context) {
-//     return Container(
-//        child:  Scaffold(
-//         appBar: AppBar(title: Text('获取微波数据'),),
-//         body: Container(
-//           child: Column(
-//             children: <Widget>[
-//               RaisedButton(
-//                 onPressed: _choiceAction,
-//                 child: Text('发送'),
-//               ),
-//               Text(
-//                 showText,
-//                 overflow: TextOverflow.ellipsis,
-//                 maxLines: 3,
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   void _choiceAction(){
-//     print("jjjjjjjjjjjjjjjjjjjjjj");
-
-//       getHttp(typeController.text.toString()).then((val){
-//         setState(() {
-//           print(val);
-//           showText = val.toString();
-//         });
-//       });
-//   }
-
-//   Future getHttp(String typedText) async{
-//     try {
-//       Response response;
-//       Options options = Options(headers: httpHeader);
-//       response = await Dio().post("https://m.weibo.cn/api/container/getIndex?type=uid&value=2803301701&containerid=1005052803301701",options: options);
-//       print(response);
-//       return response.data;
-//     } catch (e) {
-//       print(e);
-//     } 
-//   }
-// }
